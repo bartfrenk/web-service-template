dffeb90ec655d90acec0777e639c8daf2040de61
Bart Frenk <bart.frenk@gmail.com>
2017-02-12 23:30:19 +0100

    Print error message when no port specified

--- src/boot/new/web_service.clj
+++ src/boot/new/web_service.clj
@@ -13,21 +13,23 @@
 
 (defn web-service
   "Creates a minimal web service."
-  [name port]
-  (let [ns name
-        data {:name name
-              :path (b/name-to-path name)
-              :ns ns
-              :port port
-              :label (make-label name)}]
-    (println "Creating web service:" name)
-    (b/->files data
-               ["build.boot" (render "build.boot" data)]
-               ["src/{{path}}/core.clj" (render "core.clj" data)]
-               ["src/{{path}}/config.clj" (render "config.clj" data)]
-               ["src/{{path}}/meta.clj" (render "meta.clj" data)]
-               ["src/{{path}}/web/api.clj" (render "web/api.clj" data)]
-               ["src/{{path}}/web/server.clj" (render "web/server.clj" data)]
-               ["config/config.edn" (render "config.edn" data)]
-               ["test/{{path}}/core_test.clj" (render "core_test.clj" data)])))
+  [name & args]
+  (if-let [port (first args)]
+    (let [ns name
+          data {:name name
+                :path (b/name-to-path name)
+                :ns ns
+                :port port
+                :label (make-label name)}]
+      (println "Creating web service:" name)
+      (b/->files data
+                 ["build.boot" (render "build.boot" data)]
+                 ["src/{{path}}/core.clj" (render "core.clj" data)]
+                 ["src/{{path}}/config.clj" (render "config.clj" data)]
+                 ["src/{{path}}/meta.clj" (render "meta.clj" data)]
+                 ["src/{{path}}/web/api.clj" (render "web/api.clj" data)]
+                 ["src/{{path}}/web/server.clj" (render "web/server.clj" data)]
+                 ["config/config.edn" (render "config.edn" data)]
+                 ["test/{{path}}/core_test.clj" (render "core_test.clj" data)]))
+    (println "Missing -a PORT")))
 
